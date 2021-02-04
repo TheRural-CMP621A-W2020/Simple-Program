65793630aef293cdd85681017b4d985824e20ded
Andrew Pauls <ajpauls@cloud.edu.pe.ca>
2021-02-04 16:36:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+##Simple Program
+#Name: ____
+
+## Create a Simple Program that does the following.
+
+#1. Asks the user a skill question and compares their guess to the answer.   Use a loop to get the user to guess until correct.
+
+
+#2. Write a function that takes a word and a number 'n'. Print the word out 'n' times. 
+#ex. "Hello"  3 = "HelloHelloHello"
